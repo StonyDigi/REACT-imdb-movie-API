22efd7c51ebe5d7f7f2012dbe7ed26136f1a75ee
Somossy László <stony86@gmail.com>
2023-03-01 23:49:32 +0100

    utolso update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Movie from './components/Movie';
+
+/*
+  name - offical
+  currencies - huf -> name
+  capital
+  region
+  subregion
+  languages
+  borders
+  flags - kép
+
+
+*/
 
 function App() {
+
+    const [movie, setMovie] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const getMovie = () => {
+        axios.get(`https://www.omdbapi.com/?t=${search}&apikey=caada9e`)
+        .then(res => setMovie(res.data))
+    
+      }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <div className="search-header">
+        
+      <h1>OMDB Movie API</h1>
+
+      <input type="text" placeholder='Írd be a film címét' onChange={(e) => {setSearch(e.target.value)}}/>
+
+      <button onClick={getMovie}>Keresés</button>
+        
+      </div>
+      
+      <div className='movie-container'>
+      <Movie poster={movie.Poster} title={movie.Title} year={movie.Year} actors={Movie.Actors} genre={movie.Genre} imdb={movie.imdbRating} metascore={movie.Metascore} plot={movie.Plot} />
     </div>
+
+    <footer>&copy; Created by Somossy László</footer>
+
+</div>
   );
 }
 
